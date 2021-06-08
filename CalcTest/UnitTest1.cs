d183a614265cf4f9a87e02cbca2d64d5d6754411
webil <webil@MSI>
2021-06-08 18:55:43 +0530

    committing code

---
+++ CalcTest/UnitTest1.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using CalculatorApp;
+
+namespace CalcTest
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        [Test]
+        public void Addition_of_two_positive_numbers()
+        {
+            double a = 10.2, b = 11.0;//arrange
+            double res = Calc.Addition(a, b);//act
+            Assert.AreEqual(res, 21.2); //assert
+        }
+        [Test]
+        public void Addition_of_two_negative_numbers()
+        {
+            double a = -5, b = -6;//arrange
+            double res = Calc.Addition(a, b);//act
+            Assert.AreEqual(res, -11); //assert
+        }
+
+        [Test]
+        public void Addition_of_one_negative_and_one_positive_number()
+        {
+            double a = -8, b = 6;//arrange
+            double res = Calc.Addition(a, b);//act
+            Assert.AreEqual(res, -2); //assert
+        }
+
+        [Test]
+        public void Subtraction_of_two_numbers()
+        {
+            double a = 8, b = 6;//arrange
+            double res = Calc.Subtraction(a,b);//act
+            Assert.AreEqual(res, 2); //assert
+        }
+
+        [Test]
+        public void Subtraction_of_two_negative_numbers()
+        {
+            double a = -9, b = -1;//arrange
+            double res = Calc.Subtraction(a, b);//act
+            Assert.AreEqual(res, -8); //assert
+        }
+
+        [Test]
+        public void Subtraction_of_one_negative_and_one_positive_number()
+        {
+            double a = -20, b = 20;//arrange
+            double res = Calc.Subtraction(a, b);//act
+            Assert.AreEqual(res, -40); //assert
+        }
+
+        [Test]
+        public void Multiplication_of_two_numbers()
+        {
+            double a = 8, b = 5;//arrange
+            double res = Calc.Multiplication(a, b);//act
+            Assert.AreEqual(res, 40); //assert
+        }
+
+        [Test]
+        public void Multiplication_of_two_negative_numbers()
+        {
+            double a = -10, b = -10;//arrange
+            double res = Calc.Multiplication(a, b);//act
+            Assert.AreEqual(res, 100); //assert
+        }
+
+        [Test]
+        public void Multiplication_of_any_number_with_zero()
+        {
+            double a = 5, b = 0;//arrange
+            double res = Calc.Multiplication(a, b);//act
+            Assert.AreEqual(res, 0); //assert
+        }
+
+        [Test]
+        public void Division_of_two_numbers()
+        {
+            double a = 12, b = 3;//arrange
+            double res = Calc.Division(a, b);//act
+            Assert.AreEqual(res, 4); //assert
+        }
+
+        [Test]
+        public void Division_of_any_number_by_zero()
+        {
+           
+            double a = 6, b = 0;//arrange
+            double res = Calc.Division(a, b);//act
+            Assert.AreEqual(res,double.PositiveInfinity); //assert
+        }
+
+        [Test]
+        public void Division_of_zero_by_any_number()
+        {
+            double a = 0, b = 7;//arrange
+            double res = Calc.Division(a, b);//act
+            Assert.AreEqual(res, 0); //assert
+        }
+    }
+}
+
---
+++ CalculatorApp/Calc.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    public class Calc
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the function to be performed");
+            Console.WriteLine("1 = Addition");
+            Console.WriteLine("2 = Subtraction");
+            Console.WriteLine("3 = Multiplication");
+            Console.WriteLine("4 = Division \n");
+            int function = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 1st Number");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter 2nd Number");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+            double res = 0;
+            switch (function)
+            {
+                case 1:
+                    {
+                        res = Addition(number1, number2);
+                        break;
+                    }
+                case 2:
+                    {
+                        res = Subtraction(number1, number2);
+                        break;
+                    }
+                case 3:
+                    {
+                        res = Multiplication(number1, number2);
+                        break;
+                    }
+                case 4:
+                    {
+                        res = Division(number1, number2);
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Invalid Function!");
+                    break;
+            }
+            Console.WriteLine("The result is {0}", res);
+            Console.ReadKey();
+        }
+
+
+        //Addition  
+        public static double Addition(double number1, double number2)
+        {
+            double result = number1 + number2;
+            return result;
+        }
+        //Substraction  
+        public static double Subtraction(double number1, double number2)
+        {
+            double result = number1 - number2;
+            return result;
+        }
+        //Multiplication  
+        public static double Multiplication(double number1, double number2)
+        {
+            double result = number1 * number2;
+            return result;
+        }
+        //Division  
+        public static double Division(double number1, double number2)
+        {
+            try
+            {
+                double result = number1 / number2;
+                return result;
+            }
+            catch (DivideByZeroException)
+            {
+                return (0);
+            }
+        }
+
+    }
+}
